14f5ad14905f55e37fd1c27fc7518b656db6eb38
augusto.yjh <augusto.yjh@antgroup.com>
2026-03-25 12:30:25 +0800

    not use symmetric_memory for prefill stage

--- python/sglang/srt/models/deepseek_common/attention_forward_methods/forward_mha.py
+++ python/sglang/srt/models/deepseek_common/attention_forward_methods/forward_mha.py
@@ -4,9 +4,6 @@ from typing import TYPE_CHECKING
 
 import torch
 
-from sglang.srt.distributed.device_communicators.pynccl_allocator import (
-    use_symmetric_memory,
-)
 from sglang.srt.distributed.parallel_state import (
     get_dcp_group,
     get_dcp_rank,
@@ -454,12 +451,12 @@ class DeepseekMHAForwardMixin:
     def _all_gather_dcp_kv_cache(self, kv_a):
         dcp_world_size = get_dcp_world_size()
         dcp_rank = get_dcp_rank()
-        with use_symmetric_memory(get_dcp_group()):
-            gathered_kv_a = torch.zeros(
-                (kv_a.shape[0] * get_dcp_world_size(), *kv_a.shape[1:]),
-                dtype=kv_a.dtype,
-                device=kv_a.device,
-            )
+        # not use symmetric_memory unless torch mem_pool updated, see https://github.com/pytorch/pytorch/issues/178138
+        gathered_kv_a = torch.zeros(
+            (kv_a.shape[0] * get_dcp_world_size(), *kv_a.shape[1:]),
+            dtype=kv_a.dtype,
+            device=kv_a.device,
+        )
         idxs = torch.arange(kv_a.shape[0] * dcp_world_size)
         mask = idxs % dcp_world_size == dcp_rank
         gathered_kv_a[mask] = kv_a
