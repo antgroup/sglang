84ba1f0e589e33f685b5a968b45ed0f58c2c2a9f
augusto.yjh <augusto.yjh@antgroup.com>
2026-04-03 17:56:52 +0800

    fix layout of new_attn_out

--- python/sglang/srt/layers/attention/utils.py
+++ python/sglang/srt/layers/attention/utils.py
@@ -1596,7 +1596,7 @@ def cp_lse_ag_out_rs(
     )
     with use_symmetric_memory(cp_group):
         # cp_attn_out is [B,H,D], we want to transpose it to [H,B,D] for the kernel, and then transpose back after correction.
-        new_output = torch.empty_like(cp_attn_out.transpose(0, 1))
+        new_output = cp_attn_out.new_empty(cp_attn_out.transpose(0, 1).shape)
     out, _ = correct_attn_out(
         cp_attn_out, lses, cp_group.rank_in_group, ctx, new_output
     )
